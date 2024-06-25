ed5d50fe0f7880c5bc1c3bb75bdb8844c90acb9f
zhhnb17 <2964777047@qq.com>
2024-06-25 13:58:58 +0800

    借阅管理表逻辑删除

--- library/library/SpringBoot/src/main/java/com/example/demo/domain/entity/LendRecord.java
+++ library/library/SpringBoot/src/main/java/com/example/demo/domain/entity/LendRecord.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
@@ -27,6 +28,7 @@ public class LendRecord implements Serializable {
     private String status;
     private Integer borrownum;
     @TableField(value = "delete_flag")
+    @TableLogic(value = "0",delval = "1")
     private Integer deleteFlag;
 
 
